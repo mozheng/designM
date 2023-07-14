3b1e428d508c79c2a51ba0e0ed656bb999665159
mozheng <daniuatchina@163.com>
2023-07-14 10:35:08 +0800

    调整模型参数表示方法

--- movenet/movenet.py
+++ movenet/movenet.py
@@ -8,29 +8,31 @@ class MoveNet(nn.Module):
         super().__init__()
         assert layers_num > 1
         self.layers_num = layers_num
-        self.affines= []
         self.H = torch.tensor(H, requires_grad=False)
         self.W = torch.tensor(W, requires_grad=False)
         
-        for _ in range(layers_num-1):
-            alpha = nn.Parameter(torch.ones(1))
-            beta = nn.Parameter(torch.ones(1))
-            tx = nn.Parameter(torch.zeros(1))
-            ty = nn.Parameter(torch.zeros(1))
-            a = torch.exp(torch.sigmoid(alpha))
-            b = torch.exp(torch.sigmoid(beta))
-            x = torch.sigmoid(tx) * self.W
-            y = torch.sigmoid(ty) * self.H
+        self.affines_param = nn.Parameter(torch.ones(self.layers_num-1 , 4))
+            
+        self.affine = torch.tanh(self.affines_param)
+        self.affine = self.gen_affine_matrix(self.affine)
+        nn.init.xavier_uniform_(self.affines_param)
 
-            self.affines.append(nn.ModuleList([a, b, x, y]))
-        
     def image_add(self, back, front, mask):
         mask_inverse = torch.bitwise_not(mask)
         back =  torch.bitwise_and(back, mask_inverse)
         return back+front
     
-    def affinemodule(self, a, b, tx, ty):
-        return torch.cat([a,-b,tx, b,a,ty],dim=0).resize(2,3)
+    def gen_affine_matrix(self, param):
+
+        alpha = param[:,0]
+        beta = param[:,1]
+        tx = param[:,2]
+        ty = param[:,3]
+        a = torch.exp(alpha)
+        b = torch.exp(beta)
+        tx = tx * self.W
+        ty = ty * self.H
+        return torch.cat([a,-b,tx, b,a,ty],dim=0).resize(2,3, self.layers_num-1).permute(2,0,1)
 
     def forward(self, background, layers_images, layers_mask_images):
         grid = F.affine_grid(self.affine, layers_mask_images.size())
@@ -41,7 +43,7 @@ class MoveNet(nn.Module):
     
 
 if __name__ == '__main__':
-    net = MoveNet(2, 480, 640)
+    net = MoveNet(3, 480, 640)
     paras = list(net.parameters())
     for num, para in enumerate(paras):
         print('number:',num)
